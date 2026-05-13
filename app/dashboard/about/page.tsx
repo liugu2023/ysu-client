2a0edbdf07f064421210a0aad5c870420620bcf9
Youwenqwq <youwenqwq@gmail.com>
2026-05-13 21:12:41 +0800

    fix: About page mobile padding, bump to v0.5.1

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -4,7 +4,7 @@ import { AboutContent } from "@/components/about-content";
 
 export default function AboutPage() {
   return (
-    <div className="mx-auto flex w-full max-w-md flex-1 flex-col">
+    <div className="mx-auto flex w-full max-w-md flex-1 flex-col px-4">
       <AboutContent />
     </div>
   );
